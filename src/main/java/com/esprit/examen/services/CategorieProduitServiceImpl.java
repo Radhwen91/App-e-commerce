29ed50e6e8516eee5a7b62865c8b44588d9ceaf9
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-10-16 14:15:21 +0100

    Update CategorieProduitServiceImpl.java

--- src/main/java/com/esprit/examen/services/CategorieProduitServiceImpl.java
+++ src/main/java/com/esprit/examen/services/CategorieProduitServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.esprit.examen.entities.CategorieProduit;
 import com.esprit.examen.repositories.CategorieProduitRepository;
-
+//test
 @Service
 public class CategorieProduitServiceImpl implements ICategorieProduitService {
 
