37f2acc748dc9adc16e06605c8b4af08e5bc89d2
ziedlahbib <lahbibmedzied@gmail.com>
2022-10-28 15:24:15 +0100

    dockercompose

--- src/test/java/com/esprit/examen/services/SecteurActiviteServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/SecteurActiviteServiceImplTest.java
@@ -1,18 +1,18 @@
 package com.esprit.examen.services;
 
-import java.util.HashSet;
-import java.util.Set;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.esprit.examen.entities.Fournisseur;
 import com.esprit.examen.entities.SecteurActivite;
 
 import lombok.extern.slf4j.Slf4j;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,12 +21,11 @@ public class SecteurActiviteServiceImplTest {
 	@Autowired
 	SecteurActiviteServiceImpl secteuractiviteservice;
 	@Test
-	public SecteurActivite addSecteurActivite( ) {
-		Set<Fournisseur> fa = (Set<Fournisseur>) new Fournisseur();
-		SecteurActivite sa = new SecteurActivite(1000L,"aa","aa",fa);
+	public void addSecteurActivite( ) {
+		SecteurActivite sa = new SecteurActivite(1000L,"aa","aa");
 		secteuractiviteservice.addSecteurActivite(sa);
-		log.info("sa:"+sa);
-		return sa;
+		log.info("s///////////////////////////////////////////////////////////////a:"+sa);
+
 	}
 
 }
