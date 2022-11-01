55293db90b14bbbe2ac239f8cfe141d79d0d37f1
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-11-01 18:01:03 +0100

    test

--- src/main/java/com/esprit/examen/converter/FactureConverter.java
+++ src/main/java/com/esprit/examen/converter/FactureConverter.java
@@ -1,23 +1,23 @@
-//package com.esprit.examen.converter;
-//
-//
-//import org.modelmapper.ModelMapper;
-//import org.springframework.stereotype.Component;
-//
-//import com.esprit.examen.entities.Facture;
-//import com.esprit.examen.entities.FactureDTO;
-//
-//@Component
-//public class FactureConverter {
-//	  public FactureDTO convertEntityToDto(Facture facutre) {
-//	        ModelMapper modelMapper = new ModelMapper();
-//	        return modelMapper.map(facutre, FactureDTO.class);
-//	        
-//	    }
-//
-//	    public Facture convertDtoToEntity(FactureDTO factureDTO) {
-//	        ModelMapper modelMapper = new ModelMapper();
-//	        return modelMapper.map(factureDTO, Facture.class);
-//	       
-//	    }
-//}
+package com.esprit.examen.converter;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.esprit.examen.entities.Facture;
+import com.esprit.examen.entities.FactureDTO;
+
+@Component
+public class FactureConverter {
+	  public FactureDTO convertEntityToDto(Facture facutre) {
+	        ModelMapper modelMapper = new ModelMapper();
+	        return modelMapper.map(facutre, FactureDTO.class);
+	        
+	    }
+
+	    public Facture convertDtoToEntity(FactureDTO factureDTO) {
+	        ModelMapper modelMapper = new ModelMapper();
+	        return modelMapper.map(factureDTO, Facture.class);
+	       
+	    }
+}
