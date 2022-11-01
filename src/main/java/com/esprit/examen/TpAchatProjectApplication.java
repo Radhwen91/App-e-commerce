46ab0852e03eafe5d5ceafc8d4c1ad5faa0f45f5
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-11-01 18:38:54 +0100

    Update TpAchatProjectApplication.java

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -1,7 +1,7 @@
 package com.esprit.examen;
 
 
-import org.modelmapper.ModelMapper;
+//import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,12 +19,14 @@ public class TpAchatProjectApplication {
 		
 		
 }
+	
+	/*
 	@Bean
 	public ModelMapper modelMapper() {
 
 	    return new ModelMapper();
 
-	  }
+	  }*/
 	
 	
 	
