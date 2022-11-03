7eefaf092409d5cb3aa733b3d56be278be3d2abd
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2022-11-03 19:41:43 +0100

    Update ClientRepository.java

--- src/main/java/com/esprit/examen/repositories/ClientRepository.java
+++ src/main/java/com/esprit/examen/repositories/ClientRepository.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.esprit.examen.entities.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 	
 	//JPQL
