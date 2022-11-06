3ef6a3ff51d4076c5023875d38f44b2cf44bd569
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-11-06 22:15:22 +0100

    dto

--- src/main/java/com/esprit/examen/entities/FactureDTO.java
+++ src/main/java/com/esprit/examen/entities/FactureDTO.java
@@ -5,6 +5,9 @@ package com.esprit.examen.entities;
 import java.util.Date;
 import java.util.Set;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
@@ -17,9 +20,51 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FactureDTO {
 	
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idFacture;
+	private float montantRemise;
+	private float montantFacture;
+	@Temporal(TemporalType.DATE)
+	private Date dateCreationFacture;
+	@Temporal(TemporalType.DATE)
+	private Date dateDerniereModificationFacture;
+	private Boolean archivee;
+	@OneToMany(mappedBy = "facture")
+	private Set<DetailFacture> detailsFacture;
+    @ManyToOne
+    @JsonIgnore
+    private Fournisseur fournisseur;
+    @OneToMany(mappedBy="facture")
+    @JsonIgnore
+    private Set<Reglement> reglements;
+	public FactureDTO(float montantRemise, float montantFacture, Date dateCreationFacture,
+			Date dateDerniereModificationFacture, Boolean archivee) {
+		this.montantRemise = montantRemise;
+		this.montantFacture = montantFacture;
+		this.dateCreationFacture = dateCreationFacture;
+		this.dateDerniereModificationFacture = dateDerniereModificationFacture;
+		this.archivee = archivee;
+		
+	}
+	
+	public FactureDTO(Long idFacture,float montantRemise, float montantFacture, Date dateCreationFacture,
+			Date dateDerniereModificationFacture, Boolean archivee) {
+	
+		this.idFacture = idFacture;
+		this.montantRemise = montantRemise;
+		this.montantFacture = montantFacture;
+		this.dateCreationFacture = dateCreationFacture;
+		this.dateDerniereModificationFacture = dateDerniereModificationFacture;
+		this.archivee = archivee;
+		
+	}
     
 }
